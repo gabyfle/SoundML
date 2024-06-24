8582aaf56fe64864ae89c27c3d960da89eaa6006
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2024-06-25 00:20:15 +0200

    Experimenting new spectral algorithm

---
+++ src/feature/specgram.ml
@@ -0,0 +1,149 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                                                           *)
+(*  Copyright (C) 2023                                                       *)
+(*    Gabriel Santamaria                                                     *)
+(*                                                                           *)
+(*                                                                           *)
+(*  Licensed under the Apache License, Version 2.0 (the "License");          *)
+(*  you may not use this file except in compliance with the License.         *)
+(*  You may obtain a copy of the License at                                  *)
+(*                                                                           *)
+(*    http://www.apache.org/licenses/LICENSE-2.0                             *)
+(*                                                                           *)
+(*  Unless required by applicable law or agreed to in writing, software      *)
+(*  distributed under the License is distributed on an "AS IS" BASIS,        *)
+(*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(*  See the License for the specific language governing permissions and      *)
+(*  limitations under the License.                                           *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Owl
+
+module Detrend = struct
+  let none = Fun.id
+end
+
+type spectral_mode = PSD | Angle | Phase | Magnitude | Complex | Default
+
+type spectral_side = OneSided | TwoSided
+
+let fftfreq (n : int) (d : float) =
+  let nslice = ((n - 1) / 2) + 1 in
+  let fhalf = Arr.linspace 0. (float_of_int nslice) nslice in
+  let shalf = Arr.linspace (-.float_of_int nslice) (-1.) nslice in
+  let v = Arr.concatenate ~axis:0 [|fhalf; shalf|] in
+  Arr.(1. /. (d *. float_of_int n) $* v)
+
+(* Ported and adapted from the spectral helper from matplotlib.mlab All credits
+   to the original matplotlib.mlab authors and mainteners *)
+let spectral_helper ?(nfft : int = 256) ?(fs : int = 2)
+    ?(window = Signal.hamming) ?(detrend = Detrend.none) ?(noverlap : int = 0)
+    ?(side = OneSided) ?(y : Audio.audio option = None) ?(mode = Default)
+    ?(pad_to = None) ?(scale_by_freq = None) (x : Audio.audio) =
+  let same_data =
+    match y with Some y -> Audio.data x = Audio.data y | None -> false
+  in
+  let pad_to = match pad_to with Some x -> x | None -> nfft in
+  assert (pad_to >= nfft) ;
+  (* TODO: make a nice exception system for the whole library *)
+  assert (noverlap < nfft) ;
+  let mode = match mode with Default -> PSD | _ -> mode in
+  if (not same_data) && mode = PSD then assert false ;
+  (* we're making copies of the data from x and y to then use in place padding
+     and operations *)
+  let x = Audio.G.copy (Audio.data x) in
+  let y =
+    match y with
+    | Some y ->
+        Audio.G.copy (Audio.data y)
+    | None ->
+        Audio.G.copy x
+  in
+  (* We're making sure the arrays are at least of size nfft *)
+  let xshp = Audio.G.shape x in
+  ( if Array.get xshp 0 < nfft then
+      let delta = nfft - Array.get xshp 0 in
+      (* we're doing this in place in hope to gain a little bit of speed *)
+      Audio.G.pad_ ~out:x ~v:0. [[0; delta - 1]; [0; 0]] x ) ;
+  (* only padding the y array if it has been provided *)
+  ( if not same_data then
+      let yshp = Audio.G.shape y in
+      if Array.get yshp 0 < nfft then
+        let delta = nfft - Array.get yshp 0 in
+        Audio.G.pad_ ~out:y ~v:0. [[0; delta - 1]; [0; 0]] y ) ;
+  let scale_by_freq =
+    match mode with
+    | PSD ->
+        false
+    | _ -> (
+      match scale_by_freq with Some x -> x | None -> true )
+  in
+  let num_freqs, scaling_factor, freq_center =
+    match (side, pad_to mod 2) with
+    | OneSided, 0 ->
+        ((pad_to / 2) + 1, 2., 0)
+    | OneSided, _ ->
+        ((pad_to + 1) / 2, 2., 0)
+    | TwoSided, 0 ->
+        (pad_to, 1., pad_to / 2)
+    | TwoSided, _ ->
+        (pad_to, 1., (pad_to - 1) / 2)
+  in
+  let window = window nfft in
+  (* reshaped window, we'll need the original one later *)
+  let rwindow = Audio.G.reshape window [|-1; 1|] in
+  let res = Audio.G.slide ~axis:0 ~window:nfft ~step:(nfft - noverlap) x in
+  Audio.G.transpose_ ~out:res res ;
+  let res = detrend res in
+  let res = Audio.G.(res * rwindow) in
+  let res = Fft.D.rfft res ~axis:0 in
+  let len = Array.get (Audio.G.shape res) 0 in
+  Audio.G.pad_ ~out:res ~v:Complex.zero [[0; pad_to - len]; [0; 0]] res ;
+  Audio.G.get_slice_ ~out:res [[0; num_freqs - 1]; []] res ;
+  let freqs = fftfreq pad_to (1. /. float_of_int fs) in
+  ( if not same_data then (
+      let res_y =
+        Audio.G.slide ~axis:0 ~window:nfft ~step:(nfft - noverlap) y
+      in
+      Audio.G.transpose_ ~out:res_y res_y ;
+      let res_y = detrend res_y in
+      let res_y = Audio.G.(res_y * window) in
+      let res_y = Fft.D.rfft res_y ~axis:0 in
+      let len = Array.get (Audio.G.shape res_y) 0 in
+      Audio.G.pad_ ~out:res_y ~v:Complex.zero [[0; pad_to - len]; [0; 0]] res_y ;
+      Audio.G.get_slice_ ~out:res_y [[0; num_freqs - 1]; []] res_y ;
+      Audio.G.conj_ ~out:res res ;
+      Audio.G.mul_ ~out:res res res_y )
+    else
+      match mode with
+      | PSD | Default ->
+          Audio.G.conj_ ~out:res res ;
+          Audio.G.mul_ ~out:res res res
+      | Magnitude ->
+          Audio.G.abs_ ~out:res res ;
+          Audio.G.scalar_mul_ ~out:res
+            Complex.{re= Audio.G.sum' window; im= 0.}
+            res
+      | Phase | Angle ->
+          let angle = Audio.G.angle res in
+          Audio.G.set_slice_ ~out:res [[0; num_freqs - 1]; []] angle res
+      | Complex ->
+          Audio.G.scalar_div_ ~out:res
+            Complex.{re= Audio.G.sum' window; im= 0.}
+            res ) ;
+  if mode = PSD then (
+    let slice = if nfft mod 2 = 0 then [[1]; [-1]] else [[1]; []] in
+    let gslice = Audio.G.get_slice slice res in
+    Audio.G.scalar_mul_ ~out:gslice Complex.{re= scaling_factor; im= 0.} gslice ;
+    Audio.G.set_slice_ ~out:res slice gslice res ;
+    if scale_by_freq then (
+      Audio.G.scalar_div_ ~out:res Complex.{re= float_of_int fs; im= 0.} res ;
+      let n = Audio.G.sum' (Audio.G.scalar_pow (float_of_int 2) window) in
+      Audio.G.scalar_div_ ~out:res Complex.{re= n; im= 0.} res )
+    else
+      let n = Float.pow (Audio.G.sum' window) 2. in
+      Audio.G.scalar_div_ ~out:res Complex.{re= n; im= 0.} res ) ;
+  (* TODO: Adjust the frequency range and the result wth a roll *)
+  (res, freqs)
