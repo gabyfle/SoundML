18b083f8b5eb536b473ba676a8e649ad29d69af8
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2024-11-08 21:53:09 +0100

    Workflow, documentation and packaging.   - Added the workflow to the repository, enabling automatic update of the online documentation   - Added some introductory .mld file for the odoc   - Modified the requirements to stick with latest version

--- src/test.ml
+++ src/test.ml
@@ -2,7 +2,7 @@ let () =
   Owl.Log.set_level Owl.Log.DEBUG ;
   Printexc.record_backtrace true ;
   let open Soundml in
-  let audio = Io.read "music_is_moving.mp3" "mp3" in
+  let audio = Io.read "test/sin_2k.wav" "wav" in
   let mel, _ = Feature.Spectral.mel_specgram audio in
   Npy.write (Audio.data audio) "audio.npy" ;
   Npy.write mel "mel.npy"
