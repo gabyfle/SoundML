80093a6cfb9c0b35713026b3b9604a08e98f9fed
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2025-03-21 20:38:26 +0100

    Adding POC of rubberband bindings
    - Added a time stretching function that works but might not be safe/optimized and that might have some bugs
    - Updated ocamlformat to version 0.27

--- src/test.ml
+++ src/test.ml
@@ -1,4 +1,5 @@
 let () =
   let open Soundml in
-  let _ = Io.read "test/sin_2k.wav" "wav" in
-  ()
+  let v = Io.read "mono.wav" "wav" in
+  let stretched = Effects.Time.time_stretch v 0.5 in
+  Io.write stretched "stretched.wav" "wav"
