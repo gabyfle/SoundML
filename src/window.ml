6212e036465c352935730bcefc3ef9878617767a
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2025-08-30 23:14:48 +0100

    Migrating the Window module to Rune

--- src/window.ml
+++ src/window.ml
@@ -21,60 +21,67 @@
 
 type window = [`Hanning | `Hamming | `Blackman | `Boxcar]
 
-let cosine_sum ?(fftbins = false) (dtype : ('a, 'b) Nx.dtype) (a : float array)
-    m =
+let cosine_sum ?(fftbins = false) (device : 'dev Rune.device)
+    (dtype : (float, 'b) Rune.dtype) (a : float array) m =
   if m < 0 then invalid_arg "Window length M must be a non-negative integer"
-  else if m = 0 then Nx.empty dtype [|0|]
-  else if m = 1 then Nx.ones dtype [|1|]
+  else if m = 0 then Rune.empty device dtype [|0|]
+  else if m = 1 then Rune.ones device dtype [|1|]
   else
     let sym = not fftbins in
     let fac =
-      if sym then Nx.linspace dtype (-.Float.pi) Float.pi m
+      if sym then Rune.linspace device dtype (-.Float.pi) Float.pi m
       else
-        let full_range = Nx.linspace dtype (-.Float.pi) Float.pi (m + 1) in
-        Nx.slice [R [0; m]] full_range
+        let full_range =
+          Rune.linspace device dtype (-.Float.pi) Float.pi (m + 1)
+        in
+        Rune.slice [R [0; m; 1]] full_range
     in
     let w =
-      if Array.length a > 0 then Nx.full dtype [|m|] a.(0)
-      else Nx.zeros dtype [|m|]
+      if Array.length a > 0 then Rune.full device dtype [|m|] a.(0)
+      else Rune.zeros device dtype [|m|]
     in
     let w =
       Array.fold_left
         (fun acc (k, coeff) ->
           if k = 0 || coeff = 0.0 then acc
           else
-            let cos_args = Nx.mul_s fac (float_of_int k) in
-            let cos_terms = Nx.cos cos_args in
-            let term = Nx.mul_s cos_terms coeff in
-            Nx.add acc term )
+            let cos_args = Rune.mul_s fac (float_of_int k) in
+            let cos_terms = Rune.cos cos_args in
+            let term = Rune.mul_s cos_terms coeff in
+            Rune.add acc term )
         w
         (Array.mapi (fun i x -> (i, x)) a)
     in
     w
 
-let hanning ?(fftbins = false) (dtype : ('a, 'b) Nx.dtype) m =
-  cosine_sum ~fftbins dtype [|0.5; 1. -. 0.5|] m
+let hanning ?(fftbins = false) (device : 'dev Rune.device)
+    (dtype : (float, 'b) Rune.dtype) m =
+  cosine_sum ~fftbins device dtype [|0.5; 1. -. 0.5|] m
 
-let hamming ?(fftbins = false) (dtype : ('a, 'b) Nx.dtype) m =
-  cosine_sum ~fftbins dtype [|0.54; 1. -. 0.54|] m
+let hamming ?(fftbins = false) (device : 'dev Rune.device)
+    (dtype : (float, 'b) Rune.dtype) m =
+  cosine_sum ~fftbins device dtype [|0.54; 1. -. 0.54|] m
 
-let blackman ?(fftbins = false) (dtype : ('a, 'b) Nx.dtype) m =
-  cosine_sum ~fftbins dtype [|0.42; 0.5; 0.08|] m
+let blackman ?(fftbins = false) (device : 'dev Rune.device)
+    (dtype : (float, 'b) Rune.dtype) m =
+  cosine_sum ~fftbins device dtype [|0.42; 0.5; 0.08|] m
 
-let boxcar (dtype : ('a, 'b) Nx.dtype) (size : int) : ('a, 'b) Nx.t =
+let boxcar (device : 'dev Rune.device) (dtype : ('a, 'b) Rune.dtype) (size : int)
+    : ('a, 'b, 'dev) Rune.t =
   if size < 0 then failwith "Window length M must be non-negative"
-  else if size = 0 then Nx.empty dtype [|0|]
-  else Nx.ones dtype [|size|]
+  else if size = 0 then Rune.empty device dtype [|0|]
+  else Rune.ones device dtype [|size|]
 
-let get (typ : window) (dtype : ('a, 'b) Nx.dtype) :
-    ?fftbins:bool -> int -> ('a, 'b) Nx.t =
+let get (typ : window) (device : 'dev Rune.device)
+    (dtype : (float, 'b) Rune.dtype) :
+    ?fftbins:bool -> int -> (float, 'b, 'dev) Rune.t =
  fun ?fftbins size ->
   match typ with
   | `Hanning ->
-      hanning ?fftbins dtype size
+      hanning ?fftbins device dtype size
   | `Hamming ->
-      hamming ?fftbins dtype size
+      hamming ?fftbins device dtype size
   | `Blackman ->
-      blackman ?fftbins dtype size
+      blackman ?fftbins device dtype size
   | `Boxcar ->
-      boxcar dtype size
+      boxcar device dtype size
