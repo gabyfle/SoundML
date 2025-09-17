9e08efdd97757ddc9e11d121552e5cc58e116044
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2025-09-18 00:36:45 +0100

    chore: cleanup code to prepare for new features (#25)
    * chore: cleanup code to prepare for new features

--- src/transform.ml
+++ src/transform.ml
@@ -23,25 +23,27 @@ let stft ?(window = `Hanning) ?(win_length = 2048) ?(center = true) ~n_fft
     ~hop_length (x : (float, 'a, 'dev) Rune.t) =
   (* Input validation *)
   if n_fft <= 0 then invalid_arg "n_fft must be positive" ;
-  if hop_length <= 0 then invalid_arg "hop_size must be positive" ;
+  if hop_length <= 0 then invalid_arg "hop_length must be positive" ;
   if win_length <= 0 then invalid_arg "win_length must be positive" ;
   if win_length > n_fft then
     invalid_arg "win_length cannot be larger than n_fft" ;
   let device = Rune.device x in
-  let fft_window =
-    Window.get window device (Rune.dtype x) ~fftbins:true win_length
-  in
+  let dtype = Rune.dtype x in
+  (* 1. Create window and pad it if necessary to n_fft *)
+  let fft_window = Window.get window device dtype ~fftbins:true win_length in
   let fft_window =
     if win_length < n_fft then
-      Utils.pad_center fft_window ~size:n_fft ~pad_value:0.0
+      let pad_width = (n_fft - win_length) / 2 in
+      Rune.pad [|(pad_width, n_fft - win_length - pad_width)|] 0.0 fft_window
     else fft_window
   in
+  (* 2. Pad the input signal for centering *)
   let x_padded =
     if center then (
-      let x_shape = Rune.shape x in
       let pad_width = n_fft / 2 in
-      let padding = Array.make (Array.length x_shape) (0, 0) in
-      padding.(Array.length x_shape - 1) <- (pad_width, pad_width) ;
+      let rank = Rune.ndim x in
+      let padding = Array.make rank (0, 0) in
+      padding.(rank - 1) <- (pad_width, pad_width) ;
       Rune.pad padding 0.0 x )
     else x
   in
@@ -51,14 +53,32 @@ let stft ?(window = `Hanning) ?(win_length = 2048) ?(center = true) ~n_fft
     invalid_arg
       (Printf.sprintf "n_fft=%d is too large for input signal of length=%d"
          n_fft signal_length ) ;
-  let y_frames =
-    Utils.frame ~axis:(-1) ~frame_length:n_fft ~hop_length x_padded
+  (* 3. Calculate number of frames *)
+  let num_frames = 1 + ((signal_length - n_fft) / hop_length) in
+  let frame_indices = Rune.arange device Rune.int32 0 num_frames 1 in
+  (* 4. Define the function to be vmapped *)
+  let process_single_frame frame_index =
+    let start = Rune.mul_s frame_index (Int32.of_int hop_length) in
+    let start_reshaped = Rune.unsqueeze ~axes:[|1|] start in
+    let offsets = Rune.arange device Rune.int32 0 n_fft 1 in
+    let indices = Rune.add start_reshaped offsets in
+    (* Dynamic slice using take. Assuming take flattens the taken dimensions. *)
+    let frame_flat = Rune.take ~axis:(-1) indices x_padded in
+    (* Reshape back to (..., num_frames, n_fft) using -1 for num_frames *)
+    let x_rank = Rune.ndim x_padded in
+    let other_dims = Array.sub (Rune.shape x_padded) 0 (x_rank - 1) in
+    let target_shape = Array.append other_dims [|-1; n_fft|] in
+    let frame = Rune.reshape target_shape frame_flat in
+    (* Apply window. *)
+    let windowed_frame = Rune.mul frame fft_window in
+    (* FFT on the last axis *)
+    Rune.rfft ~axis:(-1) windowed_frame
   in
-  let frames_shape = Rune.shape y_frames in
-  let frames_ndim = Array.length frames_shape in
-  let window_shape = Array.make frames_ndim 1 in
-  window_shape.(frames_ndim - 2) <- n_fft ;
-  let fft_window_reshaped = Rune.reshape window_shape fft_window in
-  let windowed_frames = Rune.mul y_frames fft_window_reshaped in
-  let stft_result = Rune.rfft ~axis:(-2) windowed_frames in
-  stft_result
+  (* 5. vmap the function over the frame indices *)
+  let stft_matrix = Rune.vmap process_single_frame frame_indices in
+  (* 6. Transpose to (..., freq, time) by moving the first axis (frames) to the
+     end *)
+  let rank = Rune.ndim stft_matrix in
+  let axes = Array.init rank (fun i -> if i = rank - 1 then 0 else i + 1) in
+  let stft_transposed = Rune.transpose ~axes stft_matrix in
+  stft_transposed
