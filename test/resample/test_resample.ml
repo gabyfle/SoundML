c1aed7d99f9933e614a80374024b105658180834
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2026-08-03 18:26:45 +0200

    test: gate the resampler's steady-state allocation budgets

--- test/resample/test_resample.ml
+++ test/resample/test_resample.ml
@@ -3,4 +3,4 @@ open Windtrap
 let () =
   run "Soundml.Resample"
     ( Resample_config.suite @ Resample_kernel.suite @ Resample_law.suite
-    @ Resample_gemm.suite @ Resample_quality.suite )
+    @ Resample_gemm.suite @ Resample_quality.suite @ Resample_alloc.suite )
